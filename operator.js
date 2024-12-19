059adc57e7f6a31d05556f9d662b529c17de6bcd
muhammadsaad8958 <saadq8958@gmail.com>
2024-12-19 15:00:49 +0500

    Add files via upload

---
+++ operator.js
@@ -0,0 +1,26 @@
+/*let number = 0
+if (number>0){
+    console.log("Positive number")
+}
+else if (number<0){
+    console.log("Negative number")
+}
+else{
+    console.log("Zero")
+}*/
+
+// let num = 9
+// if (num%2==0){
+//     console.log("the num is even")
+// }
+// else {
+//     console.log("the num is odd")
+// }
+
+// let greeting = "hello"
+// let name = "saad"
+// console.log(greeting+name)
+
+let age = 5
+let product = age*5
+console.log(product)
---
+++ saad.js
@@ -0,0 +1,31 @@
+let Name =" Saad"
+console.log("hello, My Name is",Name);
+
+let a = 5;
+let b = 3;
+
+let sum = a+b;
+let difference = a-b;
+let product = a*b;
+
+console.log("for numbera a =", a, "and b =", b);
+console.log("sum (a+b):", sum);
+console.log("difference (a-b):", difference);
+console.log("product (a*b):", product);
+
+let u=8;
+let number = u % 2 == 0?
+"even number":"odd number"
+console.log(number);
+
+age=16
+let y = age >= 18 ?
+" can vote": "connot vote"
+console.log(y)
+
+
+
+
+
+
+
